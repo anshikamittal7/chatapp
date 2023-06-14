8cba39b80863070e9c29bddd8a490c9ff1e74ca8
anshikamittal7 <anshikamittal34@gmail.com>
2023-06-14 19:04:31 +0530

    initial

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDJtlIwnsuoiJb9kXaOZv-vCaIHx86e1rY",
+    authDomain: "anshikaschatapp.firebaseapp.com",
+    projectId: "anshikaschatapp",
+    storageBucket: "anshikaschatapp.appspot.com",
+    messagingSenderId: "1071426054056",
+    appId: "1:1071426054056:web:9b8aae74f2eb2b298e1f9d"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
